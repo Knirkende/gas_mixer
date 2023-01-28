33aa78fc8d3abfc7ed6228d2a57ce632a205b7fc
Ole Holgernes <ole.holgernes@gmail.com>
2023-01-28 18:40:08 +0100

    change test class name and add test

--- src/test/java/io/heimen/AppTest.java
+++ src/test/java/io/heimen/AppTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Unit test for gas mixer app.
  */
-class CalculatorTests {
+class GasMixTests {
 
     @Test
     @DisplayName("21/35, empty cylinder = {123, 77, 20}")
@@ -24,6 +24,25 @@ class CalculatorTests {
         );
         GasMix gas = new GasMix(params);
         int[] expected = {123, 77, 20};
+        int[] actual = gas.getMix();
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("21/35, non-empty cylinder = {95, 59, 16}")
+    void calculatesStandardGasNonEmptyCylinder() {
+        GasParameter params = new GasParameter(
+                24,
+                50,
+                21,
+                35,
+                21,
+                21,
+                35,
+                220
+        );
+        GasMix gas = new GasMix(params);
+        int[] expected = {95, 59, 16};
         int[] actual = gas.getMix();
         Assert.assertArrayEquals(expected, actual);
     }
