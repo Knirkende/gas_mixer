28bd4ec5db37c4d0edcfa38ad56dd0001363fbd9
Ole Holgernes <ole.holgernes@gmail.com>
2023-01-28 19:28:42 +0100

    remove debug prints

--- src/main/java/io/heimen/GasMix.java
+++ src/main/java/io/heimen/GasMix.java
@@ -28,19 +28,15 @@ public class GasMix {
     private double getTopMixPressure() {
         double targetNitrogenBar = (
                 (100 - params.targetOxygen() - params.targetHelium()) / 100) * params.targetPressure();
-        System.out.println(targetNitrogenBar);
         double currentNitrogenBar = (
                 (100 - params.currentOxygen() - params.currentHelium()) / 100) * params.currentPressure();
-        System.out.println(currentNitrogenBar);
         double missingNitrogenBar = targetNitrogenBar - currentNitrogenBar;
         return missingNitrogenBar / ((100 - params.topMixOxygen()) / 100);
     }
     // getMix returns int[] of length 3: topMix, helium, oxygen
     public int[] getMix() {
         int missingHelium = (int) Math.round(getHeliumPressure());
-        System.out.println(missingHelium);
         int missingTopMix = (int) Math.round(getTopMixPressure());
-        System.out.println(missingTopMix);
         int missingOxygen = (int) params.targetPressure() - missingHelium - missingTopMix;
         int[] result = {missingTopMix, missingHelium, missingOxygen};
         return result;
