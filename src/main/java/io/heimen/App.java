f8d70b61a1b61ec7255a837dbe7206ed87869d84
Ole Holgernes <ole.holgernes@gmail.com>
2023-01-29 17:47:43 +0100

    WIP: experimental swing layout

--- src/main/java/io/heimen/App.java
+++ src/main/java/io/heimen/App.java
@@ -1,5 +1,8 @@
 package io.heimen;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LayoutTest f = new LayoutTest();
+    }
+}
+
+class LayoutTest {
+    JFrame f;
+
+    public LayoutTest() {
+        f = new JFrame("Test layout");
+        JButton b1=new JButton("1");
+        JButton b2=new JButton("2");
+        JButton b3=new JButton("3");
+        JButton b4=new JButton("4");
+
+        f.add(b1);
+        f.add(b2);
+        f.add(b3);
+        f.add(b4);
+
+        f.setLayout(new GridLayout(2,2));
+        f.setSize(300, 300);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
